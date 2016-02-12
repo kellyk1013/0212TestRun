254a53f944a2628c73dbba901122a740669276bd
kellyk1013 <damju.kim.222@my.csun.edu>
2016-02-12 12:22:30 -0800

    update01
    omit unnecessary information

--- src/trial0212/HelloWorld.java
+++ src/trial0212/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!!");
 		System.out.println("Today is Feb. 12, 2016");
-		System.out.println("And I am not exactly having a good day.");
+		
 
 	}
 
