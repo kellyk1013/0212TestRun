3c4aeae511465e52df5bce216f64a0754952fe7c
genome3po <jeffrey.rubi.885@my.csun.edu>
2016-02-12 14:41:01 -0800

    let's go hiking!

--- src/trial0212/HelloWorld.java
+++ src/trial0212/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!!");
 		System.out.println("Today is Feb. 12, 2016");
-		
+		//editing from pc
+		System.out.println("I am going to have a good day!");
 
 	}
 
