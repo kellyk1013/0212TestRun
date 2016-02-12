00a896a7fd5ffe60d27970d4eda67f5a8ac5bca5
kellyk1013 <damju.kim.222@my.csun.edu>
2016-02-12 12:19:58 -0800

    Initial Commit
    Under if it will work

---
+++ src/trial0212/HelloWorld.java
@@ -0,0 +1,12 @@
+package trial0212;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!!");
+		System.out.println("Today is Feb. 12, 2016");
+		System.out.println("And I am not exactly having a good day.");
+
+	}
+
+}
